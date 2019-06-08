14f9b7c9dc8724cfcb621c652ae6ca2957bbe9bc
RyuHan-Min <jin74137@gmail.com>
2019-06-09 00:59:41 +0900

    new homepage

---
+++ BurgerHuman/WebContent/new/burger_JSON.js
@@ -0,0 +1,114 @@
+eqfeed_callback({
+    "type": "FeatureCollection",
+    "features": [{
+        "type": "Feature",
+        "properties": {
+			"name":"place1",
+            "place": "청주시 복대동 2930",
+			"Phone": "010-3049-1234",
+            "types": ",dyfi,eq-location-map,general-link,geoserve,historical-moment-tensor-map,historical-seismicity-map,nearby-cities,origin,p-wave-travel-times,phase-data,scitech-link,tectonic-summary,"
+        },
+        "geometry": {"type": "Point", "coordinates": [ 127.458195,36.634284, 111.16]},
+        "id": "usc000csx3"
+    },
+		{
+            "type": "place2",
+            "properties": {
+                "name":"place2",
+				"place": "청주시 복대동 2930",
+				"Phone": "010-3049-1234",
+                "types": ",dyfi,eq-location-map,geoserve,historical-moment-tensor-map,historical-seismicity-map,losspager,nearby-cities,origin,p-wave-travel-times,phase-data,scitech-link,shakemap,tectonic-summary,"
+            },
+            "geometry": {"type": "Point", "coordinates": [ 127.457370,36.633756, 544.19]},
+            "id": "usc000csw5"
+        },
+			{
+            "type": "Feature",
+            "properties": {
+				"name":"place3",
+				"place": "청주시 복대동 2930",
+				"Phone": "010-3049-1234",
+				"types": ",dyfi,eq-location-map,geoserve,historical-moment-tensor-map,historical-seismicity-map,losspager,nearby-cities,origin,p-wave-travel-times,phase-data,scitech-link,shakemap,tectonic-summary,"
+            },
+            "geometry": {"type": "Point", "coordinates": [ 127.458231,36.632954, 544.19]},
+            "id": "usc000csw5"
+        },
+				{
+            "type": "Feature",
+            "properties": {
+                "name":"place4",
+				"place": "청주시 복대동 2930",
+				"Phone": "010-3049-1234",
+                "types": ",dyfi,eq-location-map,geoserve,historical-moment-tensor-map,historical-seismicity-map,losspager,nearby-cities,origin,p-wave-travel-times,phase-data,scitech-link,shakemap,tectonic-summary,"
+            },
+            "geometry": {"type": "Point", "coordinates": [127.459376,36.632521,  544.19]},
+            "id": "usc000csw5"
+        },
+				{
+            "type": "Feature",
+            "properties": {
+                "name":"place5",
+				"place": "청주시 복대동 2930",
+				"Phone": "010-3049-1234",
+                "types": ",dyfi,eq-location-map,geoserve,historical-moment-tensor-map,historical-seismicity-map,losspager,nearby-cities,origin,p-wave-travel-times,phase-data,scitech-link,shakemap,tectonic-summary,"
+            },
+            "geometry": {"type": "Point", "coordinates": [127.449509,36.626054,  544.19]},
+            "id": "usc000csw5"
+        },
+				{
+            "type": "Feature",
+            "properties": {
+                "name":"place6",
+				"place": "청주시 복대동 2930",
+				"Phone": "010-3049-1234",
+                "types": ",dyfi,eq-location-map,geoserve,historical-moment-tensor-map,historical-seismicity-map,losspager,nearby-cities,origin,p-wave-travel-times,phase-data,scitech-link,shakemap,tectonic-summary,"
+            },
+            "geometry": {"type": "Point", "coordinates": [127.448154,36.624777,  544.19]},
+            "id": "usc000csw5"
+        },
+				{
+            "type": "Feature",
+            "properties": {
+                "name":"place7",
+				"place": "청주시 복대동 2930",
+				"Phone": "010-3049-1234",
+                "types": ",dyfi,eq-location-map,geoserve,historical-moment-tensor-map,historical-seismicity-map,losspager,nearby-cities,origin,p-wave-travel-times,phase-data,scitech-link,shakemap,tectonic-summary,"
+            },
+            "geometry": {"type": "Point", "coordinates": [127.449358,36.622843,  544.19]},
+            "id": "usc000csw5"
+        },
+				{
+            "type": "Feature",
+            "properties": {
+                "name":"place8",
+				"place": "청주시 복대동 2930",
+				"Phone": "010-3049-1234",
+                "types": ",dyfi,eq-location-map,geoserve,historical-moment-tensor-map,historical-seismicity-map,losspager,nearby-cities,origin,p-wave-travel-times,phase-data,scitech-link,shakemap,tectonic-summary,"
+            },
+            "geometry": {"type": "Point", "coordinates": [127.456736,36.622256,  544.19]},
+            "id": "usc000csw5"
+        },
+				{
+            "type": "Feature",
+            "properties": {
+                "name":"place9",
+				"place": "청주시 복대동 2930",
+				"Phone": "010-3049-1234",
+				"link": "<a href='delivery.html'>주문하기</a>",
+                "types": ",dyfi,eq-location-map,geoserve,historical-moment-tensor-map,historical-seismicity-map,losspager,nearby-cities,origin,p-wave-travel-times,phase-data,scitech-link,shakemap,tectonic-summary,"
+            },
+            "geometry": {"type": "Point", "coordinates": [127.461492,36.621637,  544.19]},
+            "id": "usc000csw5"
+        },
+				{
+            "type": "Feature",
+            "properties": {
+				"name":"place10",
+				"place": "청주시 복대동 2930",
+				"Phone": "010-3049-1234",
+                "types": ",dyfi,eq-location-map,geoserve,historical-moment-tensor-map,historical-seismicity-map,losspager,nearby-cities,origin,p-wave-travel-times,phase-data,scitech-link,shakemap,tectonic-summary,"
+            },
+            "geometry": {"type": "Point", "coordinates": [127.462610,36.624434,  544.19]},
+            "id": "usc000csw5"
+        }]
+});
